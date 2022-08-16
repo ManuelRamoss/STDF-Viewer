9f100e902e7bcddf0e6b339a6a935cefa3c170bd
jose.rendon@nxp.com <jose.rendon@nxp.com>
2022-08-16 16:13:20 +0200

    test

--- deps/cystdf/CustonExecutor.py
+++ deps/cystdf/CustonExecutor.py
@@ -1,22 +1,27 @@
 import _cystdf
+import _cystdf.signal4Analyzer
 import sys
+import numpy as np
+
+class SelfObject:
+  self = {}
+  site_cb_dict = {}
+  head_cb_dict = {}
+  availableSites = []
+  availableHeads = []
+  testRecTypeDict = {}
+  waferInfoDict = {}
+  failCntDict = {}
+  dutArray = np.array([]) 
+  signals = signal4Analyzer()
+  resultSignal = self.signals.resultSignal
+  progressSignal = self.signals.progressSignal
+  finishSignal = self.signals.finishSignal
+  stdPath='test_stdf.stdf.gz'
 
 class NXPCustom ():
       # dict to store site/head checkbox objects
-    self.site_cb_dict = {}
-    self.head_cb_dict = {}
-    self.availableSites = []
-    self.availableHeads = []
-    self.testRecTypeDict = {}
-    self.waferInfoDict = {}
-    self.failCntDict = {}
-    self.dutArray = np.array([]) 
-    self.signals = signal4Analyzer()
-    self.resultSignal = self.signals.resultSignal
-    self.progressSignal = self.signals.progressSignal
-    self.finishSignal = self.signals.finishSignal
-    self.stdPath='test_stdf.stdf.gz'
-
-
-atdf=_cystdf.analyzeSTDF(sys.argv[1],self.signal,None,None)
-print(atdf)
+    self =  SelfObject()
+    def __init__(self):
+      atdf=_cystdf.analyzeSTDF(sys.argv[1],self.signal,None,None)
+      print(atdf)
