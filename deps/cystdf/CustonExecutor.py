08dc718153a59754304b54f4a7a549449caad4c3
jose.rendon@nxp.com <jose.rendon@nxp.com>
2022-08-16 16:21:50 +0200

    t

--- deps/cystdf/CustonExecutor.py
+++ deps/cystdf/CustonExecutor.py
@@ -28,9 +28,8 @@ class SelfObject:
   finishSignal = signals.finishSignal
   stdPath='test_stdf.stdf.gz'
 
-class NXPCustom ():
       # dict to store site/head checkbox objects
-    self =  SelfObject()
-    def __init__(self):
-      atdf=_cystdf.analyzeSTDF(sys.argv[1],self.signal,None,None)
-      print(atdf)
+self =  SelfObject()
+def __init__(self):
+  atdf=_cystdf.analyzeSTDF(sys.argv[1],self.signal,None,None)
+  print(atdf)
